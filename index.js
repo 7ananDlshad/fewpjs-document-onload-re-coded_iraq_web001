d4a0dce9538c395119a12f71006c81091ce96e1e
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-15 10:51:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
-  document.getElementById().innerHTML="This is really cool!";
+  document.getElementById("text").innerHTML="This is really cool!";
 });
  
 console.log(
